4e3ab2b535813d0fbd73195745dc0eaee8e85d5d
Alexander Blinkov <tingol88@yandex.ru>
2021-04-30 19:35:12 +0300

    form

--- client/src/components/RegisterForm/RegisterForm.tsx
+++ client/src/components/RegisterForm/RegisterForm.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import TextField from "@material-ui/core/TextField";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
-import { Grid } from "@material-ui/core";
-import { AccountCircle } from "@material-ui/icons";
+import { Avatar, Box, Button, Checkbox, Container, CssBaseline, FormControlLabel, Grid, Link, Typography } from "@material-ui/core";
+import { AccountCircle, Copyright } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -11,6 +11,23 @@ const useStyles = makeStyles((theme: Theme) =>
         margin: theme.spacing(1),
         width: "25ch",
       },
+    },
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(3),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
     },
   })
 );
@@ -39,30 +56,140 @@ const RegisterForm: React.FC<Props> = () => {
   };
   return (
     <Grid container spacing={1} alignItems="flex-end" justify="center">
-      <form className={classes.root} noValidate autoComplete="off">
-        <Grid item>
-          <AccountCircle />
-        </Grid>
-        <Grid item>
-          <TextField id="input-with-icon-grid" label="With a grid" />{" "}
-          <div>
-            <TextField
-              id="filled-name"
-              label="Name"
-              value={signUpData.name}
-              onChange={(e) => handleChangeName(e, "name")}
-              variant="filled"
-            /> {signUpData.name}
- <TextField
-              id="filled-name"
-              label="Name"
-              value={signUpData.name}
-              onChange={(e) => handleChangeName(e, "name")}
-              variant="filled"
-            />
-          </div>
-        </Grid>
-      </form>
+      <div>
+        <form
+          className={[classes.root, classes.form].join(" ")}
+          noValidate
+          autoComplete="off"
+        >
+          <Grid item>
+            <AccountCircle />
+            <Typography variant="h3" component="h2">
+              Форма регистрации
+            </Typography>
+          </Grid>
+          <Grid item>
+            <div>
+              <TextField
+                id="filled-name"
+                label="Name"
+                value={signUpData.name}
+                onChange={(e) => handleChangeName(e, "name")}
+                variant="filled"
+              />{" "}
+              <TextField
+                id="filled-surname"
+                label="Surname"
+                value={signUpData.surname}
+                onChange={(e) => handleChangeName(e, "surname")}
+                variant="filled"
+              />
+              <Grid item xs={6}></Grid>
+              <TextField
+                id="filled-email"
+                label="Email"
+                value={signUpData.email}
+                onChange={(e) => handleChangeName(e, "email")}
+                variant="filled"
+              />
+              <TextField
+                id="filled-password"
+                label="Input your password"
+                value={signUpData.password}
+                onChange={(e) => handleChangeName(e, "password")}
+                variant="filled"
+              />
+            </div>
+          </Grid>
+        </form>
+      </div>
+      {/* ======================================== */}
+      <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          {/* <LockOutlinedIcon /> */}
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="I want to receive inspiration, marketing promotions and updates via email."
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </Container>
     </Grid>
   );
 };

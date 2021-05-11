ab57af15672848ba9dc85ed83e5ef9682e342bea
Alexander Blinkov <tingol88@yandex.ru>
2021-05-11 16:30:49 +0300

    all notes back n front

--- test_codwars.js
+++ test_codwars.js
@@ -73,52 +73,52 @@
 // console.log(snail([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
 // console.log(snail([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20], [21, 22, 23, 24, 25]])); // [1, 2, 3, 4, 5, 10, 15, 20, 25, 24, 23, 22, 21, 16, 11, 6, 7, 8, 9, 14, 19, 18, 17, 12, 13]);
 
-function formatDuration(seconds) {
-  if(seconds===0) return now
-  if (seconds < 60) return `${seconds} second${seconds === 1 ? '' : 's'}`
-  let years = 0;
-  let days = 0;
-  let hours = 0;
-  let minutes = 0;
-  let secondsForResult = 0;
-  let remain = seconds;
-  const arrResult = []
-  let itemString = ''
-  if (remain >= (365 * 24 * 60 * 60)) {
-    years = Math.floor(remain / (365 * 24 * 60 * 60))
-    itemString = years + ` year${years > 1 ? 's' : ''}`
-    arrResult.push(itemString)
-    remain -= (365 * 24 * 60 * 60) * years
-  }
-  if (remain >= (24 * 60 * 60)) {
-    days = Math.floor(remain / (24 * 60 * 60))
-    itemString = days + ` day${days > 1 ? 's' : ''}`
-    arrResult.push(itemString)
-    remain -= (24 * 60 * 60) * days
-  }
-  if (remain >= (60 * 60)) {
-    hours = Math.floor(remain / (60 * 60))
-    itemString = hours + ` hour${hours > 1 ? 's' : ''}`
-    arrResult.push(itemString)
-    remain -= (60 * 60) * hours
-  }
-  if (remain >= (60)) {
-    minutes = Math.floor(remain / (60))
-    itemString = minutes + ` minute${minutes > 1 ? 's' : ''}`
-    arrResult.push(itemString)
-    remain -= (60) * minutes
-  }
-  secondsForResult = remain
-  if (secondsForResult) arrResult.push(`${secondsForResult} second${secondsForResult === 1 ? '' : 's'}`)
+// function formatDuration(seconds) {
+//   if(seconds===0) return now
+//   if (seconds < 60) return `${seconds} second${seconds === 1 ? '' : 's'}`
+//   let years = 0;
+//   let days = 0;
+//   let hours = 0;
+//   let minutes = 0;
+//   let secondsForResult = 0;
+//   let remain = seconds;
+//   const arrResult = []
+//   let itemString = ''
+//   if (remain >= (365 * 24 * 60 * 60)) {
+//     years = Math.floor(remain / (365 * 24 * 60 * 60))
+//     itemString = years + ` year${years > 1 ? 's' : ''}`
+//     arrResult.push(itemString)
+//     remain -= (365 * 24 * 60 * 60) * years
+//   }
+//   if (remain >= (24 * 60 * 60)) {
+//     days = Math.floor(remain / (24 * 60 * 60))
+//     itemString = days + ` day${days > 1 ? 's' : ''}`
+//     arrResult.push(itemString)
+//     remain -= (24 * 60 * 60) * days
+//   }
+//   if (remain >= (60 * 60)) {
+//     hours = Math.floor(remain / (60 * 60))
+//     itemString = hours + ` hour${hours > 1 ? 's' : ''}`
+//     arrResult.push(itemString)
+//     remain -= (60 * 60) * hours
+//   }
+//   if (remain >= (60)) {
+//     minutes = Math.floor(remain / (60))
+//     itemString = minutes + ` minute${minutes > 1 ? 's' : ''}`
+//     arrResult.push(itemString)
+//     remain -= (60) * minutes
+//   }
+//   secondsForResult = remain
+//   if (secondsForResult) arrResult.push(`${secondsForResult} second${secondsForResult === 1 ? '' : 's'}`)
 
-  if (arrResult.length > 2) {
-    const last = arrResult.pop()
-    return `${arrResult.join(', ')} and ${last}`
-  } else if (arrResult.length === 2) return arrResult[0] + ' and ' + arrResult[1]
-  else return arrResult[0]
-}
+//   if (arrResult.length > 2) {
+//     const last = arrResult.pop()
+//     return `${arrResult.join(', ')} and ${last}`
+//   } else if (arrResult.length === 2) return arrResult[0] + ' and ' + arrResult[1]
+//   else return arrResult[0]
+// }
 
-console.log(formatDuration(3600));
+// console.log(formatDuration(3600));
 
 // Test.assertEquals(formatDuration(1), "1 second");
 // Test.assertEquals(formatDuration(62), "1 minute and 2 seconds");
@@ -126,4 +126,18 @@ console.log(formatDuration(3600));
 // Test.assertEquals(formatDuration(3600), "1 hour");
 // Test.assertEquals(formatDuration(3662), "1 hour, 1 minute and 2 seconds");
 
+function solution(list) {
+  const result = []
+  for (let i = 0; i < list.length; i += 1) {
+    if(list[i] === list[i-1]+1 && list[i] === list[i+1]-1  ) {
+      result.push('!')
+    } else {
+      result.push(list[i])
+    }
+  }
+  return result.join(',').replace(/(,!){2,}/gm, '!').replace(/(,!,)/gi, '!').replace(/(!,)/gi, '!').replace(/!/gi, '-')
+ }
+
+console.log(solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]))
+// "-6,-3-1,3-5,7-11,14,15,17-20"));
 
